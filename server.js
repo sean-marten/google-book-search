cc980b7a19825a8ed483554d1675e6f410803db6
smarten <smarten.developer@gmail.com>
2020-11-29 20:44:15 -0800

    hmm

--- server.js
+++ server.js
@@ -14,15 +14,12 @@ mongoose.set("debug", true);
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-app.use(express.static("public"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
-
-app.get("*", (_req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
